1b8e427f130873c1951318fd31c4be459c557032
RobinHellemans <robin.hellemans@fourcast.io>
2016-10-14 17:41:03 +0200

    Completed part 3

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,40 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider) {
+    var config = {
+      apiKey: "AIzaSyARTAzLNSX3-Q3A2Bc_xrbA8GfkYUIwGh0",
+      authDomain: "contacts-da438.firebaseapp.com",
+      databaseURL: "https://contacts-da438.firebaseio.com",
+      storageBucket: "contacts-da438.appspot.com",
+      messagingSenderId: "724031356081"
+    };
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        default: config.databaseURL + '/contacts'
+      });
+    firebase.initializeApp(config);
+  })
+  .run(function($transitions, $state, AuthService) {
+    $transitions.onStart({
+      to: function(state) {
+        return !!(state.data && state.data.required);
+      }
+    }, function() {
+      return AuthService
+        .requireAuthentication()
+        .catch(function() {
+          return $state.target('auth.login');
+        });
+    });
+
+    $transitions.onStart({
+      to: 'auth.*'
+    }, function() {
+      if (AuthService.isAuthenticated()) {
+        return $state.target('app');
+      }
+    });
+  });
